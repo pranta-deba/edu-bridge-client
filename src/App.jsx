82287bb5fb4e5544fac257c05180e8cfe0753aae
Pranto Deb <rajprit0m@outlook.com>
2024-09-17 22:44:38 +0600

    [Project Setup] : Instal vite app and dependency : material ui, axios, react router dom, react icons, tailwind css etc

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Button from '@mui/material/Button'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <Button variant="contained" color="primary">
+        click
+      </Button>
+    </>
+  )
+}
+
+export default App
